a4889f63b35e611a1aa17718116cfdd34f06173c
esmabalkan <esmanurbalkan15@gmail.com>
2023-03-29 23:22:11 +0300

    Homework1
    python selenium

---
+++ homework1.py
@@ -0,0 +1,19 @@
+# Pyhton Veri Tipleri
+
+# Text Type: String, metinsel ifadeler  için kullanılır.
+
+# Numeric Type: İnteger, Float, Complex. Sayısal ifadeler için kullanılır. Tam sayı, ondalıklı sayı vs. 
+
+# Boolen Type: True veya False, kontrol sağlayan yapılarda kullanılır. Bu iki cevaptan birini döndürür.
+
+ 
+userName = "esma"
+userPaswrd = "123456789"
+
+loginUserName = "esma"
+loginUserPaswrd = "123456789"
+
+if userName == loginUserName and userPaswrd == loginUserPaswrd:
+    print("işleminiz başarıyla gerçekleşti")
+else:
+    print("İşleminiz gerçekleştirelemedi. Bilgilerinizi kontrol edin.")
